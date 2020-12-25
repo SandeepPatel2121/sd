f1541ad8f8a8e70a46a5c024fcac1025e627cb5a
SUPatel <patel1919ps@gmail.com>
2020-12-25 13:23:02 +0530

    initial commit

---
+++ src/main/java/com/nestlings/NestlingsApplication.java
@@ -0,0 +1,13 @@
+package com.nestlings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestlingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NestlingsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nestlings/NestlingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nestlings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NestlingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
