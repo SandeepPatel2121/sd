41678b8747f6aa3f8207976814ed8efb2049cdb5
SUPatel <patel1919ps@gmail.com>
2020-12-26 16:41:21 +0530

    changed spring boot version from 2.4.1 to 2.3.0

--- src/main/java/com/nestlings/entity/util/ResponseModel.java
+++ src/main/java/com/nestlings/entity/util/ResponseModel.java
@@ -1,5 +1,6 @@
 package com.nestlings.commonentity.utils;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,6 +10,7 @@ import lombok.Setter;
  */
 @Getter
 @Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ResponseModel {
 
     private int status;
