02936511235a53a4b9389c702e5620b10a8f4fcd
SUPatel <patel1919ps@gmail.com>
2020-12-28 17:53:10 +0530

    added exception and type

---
+++ src/main/java/com/nestlings/entities/exceptions/NestlingsException.java
@@ -0,0 +1,36 @@
+package com.nestlings.entities.exceptions;
+
+import com.nestlings.entities.enums.NestlingsExceptionType;
+
+/**
+ *
+ * @author bypt-dev-laptop-9
+ */
+public class NestlingsException extends RuntimeException {
+    
+    protected NestlingsExceptionType errorCode;
+    protected String errorMessage;
+    
+    public NestlingsException(NestlingsExceptionType type, RuntimeException runtimeException) {
+        super(runtimeException.getMessage());
+        this.errorCode = type;
+        this.errorMessage = runtimeException.getMessage();
+    }
+
+    public NestlingsExceptionType getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(NestlingsExceptionType errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+    
+}
