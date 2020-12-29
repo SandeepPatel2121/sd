496922e887e1b9ab652be58f86b868ab3a522858
vishal mahesuriya <vishal.mahesuriya@bypeopletechnologies.com>
2020-12-29 12:35:28 +0530

    add Forget password

---
+++ src/main/java/com/nestlings/member/ForgotPassword.java
@@ -0,0 +1,29 @@
+
+package com.nestlings.member;
+
+/**
+ *
+ * @author ú
+ */
+public class ForgotPassword {
+    
+     private String emailId;
+    private String password;
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String updatePassword) {
+        this.password = updatePassword;
+    }
+    
+}
