b5c4db0f988999aa19cfa82a6a6abacf6c24c3b7
dignesh fultariya <fultariyadigneshoo7@gmail.com>
2020-12-29 16:31:59 +0530

    resolve conflict

--- src/main/java/com/nestlings/entities/utils/EmailSubscriptionRepo.java
+++ src/main/java/com/nestlings/entities/utils/EmailSubscriptionRepo.java
@@ -21,23 +21,13 @@ import java.util.Map;
  */
 @Component
 public class EmailSubscriptionRepo {
-    
-<<<<<<< HEAD
-    private final Logger LOGGER = LoggerFactory.getLogger(EmailSubscriptionRepo.class);
-=======
-        private final Logger LOGGER = LoggerFactory.getLogger(EmailSubscriptionRepo.class);
-    
-    private Map<String, EmailSubscriptionDetails> emailSubscriptionDetailsMap;
->>>>>>> b2b1fbb286d6a96d2e081d9c209c69e0280ec2c9
 
-    public EmailSubscriptionRepo() {
-        this.emailSubscriptionDetailsMap = new HashMap();
-    }
-        
+    private final Logger LOGGER = LoggerFactory.getLogger(EmailSubscriptionRepo.class);
+
+    private Map<String, EmailSubscriptionDetails> emailSubscriptionDetailsMap;
+
     @Autowired
     private NestlingsCryptoUtil nestlingsCryptoUtil;
-
-    private Map<String, EmailSubscriptionDetails> emailSubscriptionDetailsMap;
 
     public EmailSubscriptionRepo() {
         this.emailSubscriptionDetailsMap = new HashMap();
@@ -118,7 +108,6 @@ public class EmailSubscriptionRepo {
         }
         return decryptedEmail;
     }
-<<<<<<< HEAD
 
     public Map<String, EmailSubscriptionDetails> getEmailSubscriptionDetailsMap() {
         return emailSubscriptionDetailsMap;
@@ -127,43 +116,5 @@ public class EmailSubscriptionRepo {
     public void setEmailSubscriptionDetailsMap(Map<String, EmailSubscriptionDetails> emailSubscriptionDetailsMap) {
         this.emailSubscriptionDetailsMap = emailSubscriptionDetailsMap;
     }
-=======
-    
-    public boolean isSubscribedToSendEmailByUnsubscribeToken(String unsubscribeToken) {
-        return isSubscribedToSendEmail(this.getDecryptedEmail(unsubscribeToken));
-    }
 
-    public boolean isSubscribedToSendEmail(String emailId) {
-        boolean subscribed = true;
-        try {
-            if (hasEmailSubscriptionDetailsByUserEmail(emailId) && getEmailSubscriptionDetailsByUserEmail(emailId) != null
-                    && this.getEmailSubscriptionDetailsByUserEmail(emailId).getStatusId() == 2) {
-                subscribed = false;
-            }
-        } catch (Exception e) {
-            LOGGER.error(e.getMessage(), e);
-        }
-        return subscribed;
-    }
-    
-    public EmailSubscriptionDetails getEmailSubscriptionDetailsByUserEmail(String emailId) {
-        return this.emailSubscriptionDetailsMap.get(emailId);
-    }
-    
-    public String getDecryptedEmail(String encryptedEmail) {
-        String decryptedEmail = null;
-        try {
-            decryptedEmail = nestlingsCryptoUtil.decrypt(encryptedEmail);
-        } catch (Exception e) {
-            LOGGER.error(e.getMessage(), e);
-            //throw new NestlingsException(NestlingsExceptionType.RUNTIME_EXCEPTION, e.getMessage());
-        }
-        return decryptedEmail;
-    }
-    
-    public boolean hasEmailSubscriptionDetailsByUserEmail(String emailId) {
-        return this.emailSubscriptionDetailsMap.containsKey(emailId);
-    }
-    
->>>>>>> b2b1fbb286d6a96d2e081d9c209c69e0280ec2c9
 }
