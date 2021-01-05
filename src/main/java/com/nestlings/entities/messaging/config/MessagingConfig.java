0f56bac46e0ebd765d0c744ce553465943e4f9c7
SUPatel <patel1919ps@gmail.com>
2021-01-05 13:31:28 +0530

    added rabbitMQ source config

--- src/main/java/com/nestlings/entities/messaging/config/MessagingConfig.java
+++ src/main/java/com/nestlings/entities/messaging/config/MessagingConfig.java
@@ -7,13 +7,16 @@ import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 /**
  *
@@ -21,8 +24,33 @@ import org.springframework.context.annotation.Configuration;
  */
 @EnableRabbit
 @Configuration
+@PropertySource("classpath:application.properties")
 public class MessagingConfig {
+
+    @Value("${spring.rabbitmq.host}")
+    private String HOST;
     
+    @Value("${spring.rabbitmq.port}")
+    private Integer PORT;
+    
+    @Value("${spring.rabbitmq.username}")
+    private String USERNAME;
+    
+    @Value("${spring.rabbitmq.password}")
+    private String PASSWORD;
+    
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
+        
+        connectionFactory.setHost(HOST);
+        connectionFactory.setPort(PORT);
+        connectionFactory.setUsername(USERNAME);
+        connectionFactory.setPassword(PASSWORD);
+        
+        return connectionFactory;
+    }
+
     @Bean
     Queue messaginQueue() {
         return new Queue(EntityConstants.MESSAGING_MESSAGING_QUEUE, false);
@@ -32,22 +60,22 @@ public class MessagingConfig {
     Queue commonQueue() {
         return new Queue(EntityConstants.MESSAGING_COMMON_QUEUE, false);
     }
-    
+
     @Bean
     DirectExchange exchange() {
         return new DirectExchange(EntityConstants.MESSAGING_EXCHANGE);
     }
-    
+
     @Bean
-    Binding messageBinding(@Qualifier("messaginQueue")Queue queue, DirectExchange exchange) {
+    Binding messageBinding(@Qualifier("messaginQueue") Queue queue, DirectExchange exchange) {
         return BindingBuilder.bind(queue).to(exchange).with(EntityConstants.MESSAGING_MESSAGING_ROUTING);
     }
 
     @Bean
-    Binding commonBinding(@Qualifier("commonQueue")Queue queue2, DirectExchange exchange) {
+    Binding commonBinding(@Qualifier("commonQueue") Queue queue2, DirectExchange exchange) {
         return BindingBuilder.bind(queue2).to(exchange).with(EntityConstants.MESSAGING_COMMON_ROUTING);
     }
-    
+
     @Bean
     public MessageConverter jsonMessageConverter() {
         return new Jackson2JsonMessageConverter();
@@ -58,5 +86,5 @@ public class MessagingConfig {
         rabbitTemplate.setMessageConverter(jsonMessageConverter());
         return rabbitTemplate;
     }
-    
+
 }
