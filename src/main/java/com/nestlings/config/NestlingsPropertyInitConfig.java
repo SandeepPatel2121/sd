bf8470a98bd7a5116bbd0c630f56e68cff59768f
vishal mahesuriya <vishal.mahesuriya@bypeopletechnologies.com>
2021-01-05 12:13:19 +0530

    Add Friend Properties

--- src/main/java/com/nestlings/config/NestlingsPropertyInitConfig.java
+++ src/main/java/com/nestlings/config/NestlingsPropertyInitConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
                 @PropertySource(value="classpath:nestlings.lookup.query.properties"),
                 @PropertySource(value="classpath:nestlings.auth.query.properties"),
                 @PropertySource(value="classpath:nestlings.university.query.properties"),
+                @PropertySource(value="classpath:nestlings.friend.query.properties"),
                @PropertySource(value="classpath:nestlings.config.properties"),
         }
 )
