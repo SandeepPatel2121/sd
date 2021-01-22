23fe94675b6c2582edea79a9aba8d239afa47a32
dignesh fultariya <fultariyadigneshoo7@gmail.com>
2021-01-22 20:08:39 +0530

    added QueryConstant for query params.

---
+++ src/main/java/com/nestlings/entities/utils/QueryConstants.java
@@ -0,0 +1,44 @@
+package com.nestlings.entities.utils;
+
+public class QueryConstants {
+
+    private QueryConstants(){}
+
+    public static final String USER_ID = "userId";
+    public static final String STATUS_ID = "statusId";
+    public static final String APPLICATION_DATA = "applicationData";
+    public static final String UPDATED_DATE = "updatedDate";
+    public static final String APPLICATION_ID = "applicationId";
+    public static final String COLLEGE_DETAILS_ID = "collegeDetailsId";
+    public static final String MESSAGE_THREAD_ID = "messageThreadId";
+    public static final String CREATED_DATE = "createdDate";
+    public static final String APPLICATION_TRACK_ID = "applicationTrackId";
+    public static final String MESSAGE_ID = "messageId";
+    public static final String USER_TYPE = "userType";
+    public static final String RATINGS = "ratings";
+    public static final String RATINGS_TEXT = "ratingsText";
+    public static final String RATINGS_ID = "ratingsId";
+    public static final String ENTITY_ID = "entityId";
+    public static final String MESSAGE = "message";
+    public static final String DUE_DATE = "dueDate";
+    public static final String CONTENT_TYPE_TAG = "contentTypeTag";
+    public static final String CREATED_BY = "createdBy";
+    public static final String CONTENT = "content";
+    public static final String EXPIRE_DATE = "expireDate";
+    public static final String TRACK_ID = "trackId";
+    public static final String APPLICATION_STATUS_ID = "applicationStatusId";
+    public static final String URL = "url";
+    public static final String CONTENT_TYPE = "contentType";
+    public static final String CONTENT_LENGTH = "contentLength";
+    public static final String FILE_NAME = "fileName";
+    public static final String NOTIFICATION_TYPE_ID = "notificationTypeId";
+    public static final String OBJECT_DETAILS = "objectDetails";
+    public static final String NOTIFICATION = "notification";
+    public static final String NOTIFICATION_ID = "notificationId";
+    public static final String UNITID = "unitid";
+    public static final String LIMIT = " LIMIT ";
+    public static final String OFFSET = " OFFSET ";
+    public static final String ORDER_BY = " order by ";
+    public static final String DESC_SPACE = "Desc ";
+    public static final String ASC_SPACE = "Asc ";
+}
