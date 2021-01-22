d7c5e8b026eb4562bea5f416ff06ac7351108948
vishal mahesuriya <vishal.mahesuriya@bypeopletechnologies.com>
2021-01-22 18:16:49 +0530

    sonar bugs fixing.

--- src/main/java/com/nestlings/entities/utils/EntityConstants.java
+++ src/main/java/com/nestlings/entities/utils/EntityConstants.java
@@ -30,5 +30,75 @@ public class EntityConstants {
     public static final String MESSAGING_MESSAGING_ROUTING = "messaging";
     public static final String MESSAGING_COMMON_ROUTING = "common";
     public static final String MESSAGING_NOTIFICATION_ROUTING = "notification";
+    
+    /*
+     * USER
+     */
+        
+    public static final String USER_ID = "userId";
+
+    /*
+     * SCORE
+     */
+    public static final String SCOREID = "scoreID";
+    public static final String SCORE_DATE = "scoreDate";
+    public static final String VERBAL = "verbal";
+    public static final String QUANTITATIVE = "quantitative";
+    public static final String WRITING = "writing";
+    public static final String TOTAL = "total";
+    
+    /*
+     * TEST_SCORE
+     */
+    public static final String TEST_SCOREID = "testScoreID";
+    public static final String TEST_DATE = "testDate";
+    public static final String ENGLISH_SCORE = "englishScore";
+    public static final String MATH_SCORE = "mathScore";
+    public static final String SCIENCE_SCORE = "scienceScore";
+    public static final String READING_SCORE = "readingScore";
+    public static final String COMPOSITE_SCORE = "compositeScore";
+    public static final String WRITING_SCORE = "writingScore";
+    
+    /*
+     * WORK
+     */
+    public static final String P_USERID = "p_userID";
+    public static final String P_COMPANYID = "p_companyID";
+    public static final String P_COMPANYNAME = "p_companyName";
+    public static final String P_COMPANYTITLE = "p_companyTitle";
+    public static final String P_COURSE = "p_course";
+    public static final String P_ADDRESSID = "p_addressID";
+    public static final String P_STREETNAME = "p_streetName";
+    public static final String P_CITY = "p_city";
+    public static final String P_STATE = "p_state";
+    public static final String P_STATEID = "p_stateID";
+    public static final String P_ZIP = "p_zip";
+    public static final String P_COUNTRY = "p_country";
+    public static final String P_COUNTRYID = "p_countryID";
+    public static final String P_STARTDATE = "p_startDate";
+    public static final String P_ENDDATE = "p_endDate";
+    public static final String P_COMPANYDESCRIPTION = "p_companyDescription";
+    
+      /*
+     * CHECKLIST
+     */
+    
+    public static final String CHECKLISTID = "checklistID";
+    public static final String SUB_CHECKLISTID = "subChecklistID";
+    public static final String CONTENT = "content";
+    public static final String TITLE = "title";
+    public static final String STATUS = "status";
+    public static final String TASK_ID = "taskID";
+    public static final String USERPREFERENCEID = "userPreferenceID";
+    public static final String USERINSTITUTIONID = "userInstitutionId";
+     public static final String COURSEID = "courseID";
+
+
+
+  
+
+
+    
+
 
 }
