d0d1237389ba73d2e53f2caf2bf1b2659c360233
dignesh fultariya <fultariyadigneshoo7@gmail.com>
2021-03-08 10:52:41 +0530

    small change

--- src/main/java/com/nestlings/member/UserPreference.java
+++ src/main/java/com/nestlings/member/UserPreference.java
@@ -1,10 +1,14 @@
 package com.nestlings.member;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserPreference {
 
     private String countryForStudy="";
