9d42895dbe90595bcbd13bac3fa8258817fb3e73
dignesh fultariya <fultariyadigneshoo7@gmail.com>
2021-03-23 19:44:42 +0530

    added app builder queries.

---
+++ src/main/java/com/nestlings/entities/search/SiteSearchResult.java
@@ -0,0 +1,12 @@
+package com.nestlings.entities.search;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SiteSearchResult {
+    private Integer entityId;
+    private String entityType;
+    private String entityName;
+}
